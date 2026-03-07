a85934a4b109a4cfa4517e09359970b6c44593ca
abdessamad najib <najibabdessamad10@gmail.com>
2026-03-07 21:21:22 +0000

    fix table ui

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -65,7 +65,7 @@
                             <td><p class="{{ $employe->status === "active" ? "text-success bg-success bg-opacity-10 rounded text-center py-1 w-20" : "text-gray bg-secondary bg-opacity-10 rounded text-center py-1 w-20" }}">{{ $employe->status }}</p></td>
                             <td>
                                 <div class="flex gap-4">
-                                    <form action="{{ route('employees.update', ['employee' => $employe]) }}" method="POST">
+                                    <form action="{{ route('employees.update', ['employee' => $employe]) }}" method="POST" class="flex items-center">
                                         @csrf
                                         @method('PUT')
                                         <button><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#999999"><path d="M482-160q-134 0-228-93t-94-227v-7l-64 64-56-56 160-160 160 160-56 56-64-64v7q0 100 70.5 170T482-240q26 0 51-6t49-18l60 60q-38 22-78 33t-82 11Zm278-161L600-481l56-56 64 64v-7q0-100-70.5-170T478-720q-26 0-51 6t-49 18l-60-60q38-22 78-33t82-11q134 0 228 93t94 227v7l64-64 56 56-160 160Z"/></svg></button>
